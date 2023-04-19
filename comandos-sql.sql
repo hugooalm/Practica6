5e344ac8ba80122eba731b5ed24a7f9532d3aa71
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2023-04-19 13:09:13 +0200

    Modificación de datos

--- comandos-sql.sql
+++ comandos-sql.sql
@@ -8,6 +8,6 @@ CREATE TABLE agenda(
     tipo_contacto varchar);
 
 -- Insercion de datos
-INSERT INTO agenda(id,nombre,apes,telefono,edad,tipo_contacto) VALUES (11,'Juan','Martín Pérez','917897898','25','TRABAJO');
+INSERT INTO agenda(id,nombre,apes,telefono,edad,tipo_contacto) VALUES (1,'Juan','Martín Pérez','917897898','25','TRABAJO');
 INSERT INTO agenda(id,nombre,apes,telefono,edad,tipo_contacto) VALUES (2,'Ana','García García','699887744','22','TRABAJO');
 INSERT INTO agenda(id,nombre,apes,telefono,edad,tipo_contacto) VALUES (3,'Marta','López Pérez','677889900','54','FAVORITO');
